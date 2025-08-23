28becb4c3a0379e2032859e1f01ddb758be32249
daniroyosole <46968249+daniroyosole@users.noreply.github.com>
2025-08-23 22:28:27 +0200

    Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,29 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['leaflet', 'react-leaflet']
-  },
-  define: {
-    global: 'globalThis'
-  },
-  build: {
-    target: 'es2015',
-    commonjsOptions: {
-      include: [/node_modules/]
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        manualChunks: {
-          leaflet: ['leaflet', 'react-leaflet']
-        }
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      'leaflet': 'leaflet/dist/leaflet.js'
-    }
-  }
 })
